13fcfcfd8dd7e331ce44952b5f9b1df752169241
Jacob Wynberg <12031542@student.uts.edu.au>
2017-01-25 17:40:10 +1100

    Added Attribute Routing Added more actions in MoviesController

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -13,7 +13,33 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() {Id = 1, Name = "Shrek!"};
-            return View();
+            return View(movie);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id = " + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+
+            if (string.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            return Content(string.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1, 12)}")]
+        public ActionResult ByReleaseYear(int year, int month)
+        {
+            return Content(year + "/" + month);
         }
     }
 }
